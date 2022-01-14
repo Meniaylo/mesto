399d2dc36ad6a3acd9e5b7ce02e0d20d8601595b
yzma <upthere@yandex.ru>
2022-01-14 14:14:39 +0300

    debug: input fixes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 let editBtn = document.querySelector('.profile__edit-btn');
 let popup = document.querySelector('.popup');
 let popupExitBtn = document.querySelector('.popup__exit-btn');
+let inputName = document.querySelector('.form__input-name');
+let inputOccupation = document.querySelector('.form__input-occupation');
+let profileName = document.querySelector('.profile__name');
+let profileOccupation = document.querySelector('.profile__occupation');
 
 
 function showPopup() {
@@ -12,3 +16,7 @@ function hidePopup() {
   popup.classList.remove('popup_active');
 }
 popupExitBtn.addEventListener('click', hidePopup);
+
+function fillPlaceholderName () {
+  
+}
