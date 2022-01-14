ed5ef1c6ccbe5b3501387209047c5795879f141b
yzma <upthere@yandex.ru>
2022-01-14 15:32:06 +0300

    feat: profile redaction

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ let popup = document.querySelector('.popup');
 let popupExitBtn = popup.querySelector('.popup__exit-btn');
 let profileName = document.querySelector('.profile__name');
 let profileOccupation = document.querySelector('.profile__occupation');
+let profileInputs = popup.querySelectorAll('input');
+let formElement = document.querySelector('.form');
 
 
 function showPopup() {
@@ -14,12 +16,18 @@ function hidePopup() {
 }
 
 function fillInput() {
-  let profileInputs = popup.querySelectorAll('input');
   profileInputs[0].value = profileName.textContent;
   profileInputs[1].value = profileOccupation.textContent;
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = profileInputs[0].value;
+  profileOccupation.textContent = profileInputs[1].value;
 }
 
 editBtn.addEventListener('click', showPopup);
 editBtn.addEventListener('click', fillInput);
 popupExitBtn.addEventListener('click', hidePopup);
-
+formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', hidePopup);
