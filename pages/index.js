ce9cf47dc93a735778e0fbe24b1f9649a4c3eb0b
yzma <upthere@yandex.ru>
2022-01-14 12:54:46 +0300

    refactor

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 let editBtn = document.querySelector('.profile__edit-btn');
 let popup = document.querySelector('.popup');
+let popupExitBtn = document.querySelector('.popup__exit-btn');
+
 
 function showPopup() {
   popup.classList.add('popup_active');
 }
 editBtn.addEventListener('click', showPopup);
-
-
-let popupExitBtn = document.querySelector('.popup__exit-btn');
 
 function hidePopup() {
   popup.classList.remove('popup_active');
