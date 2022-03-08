edfd8df379e17d7c4050a8438940580fbaec25b2
yzma <upthere@yandex.ru>
2022-03-08 15:35:54 +0300

    hotfix: structure

--- scripts/formValidationConfig.js
+++
@@ -1,8 +0,0 @@
-const formValidationConfig = {
-  formSelector: '.form',
-  inputSelector: '.form__input',
-  inputErrorClass: 'form__input-error_active',
-  inputIsInvalidClass: 'form__input_type_error',
-  submitBtnSelector: '.form__submit-btn',
-  submitBtnInactiveClass: 'form__submit-btn_inactive'
-};
