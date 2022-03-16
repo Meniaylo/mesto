52321a7e0a6067f6bdcbb91e508e994dfaa128d0
yzma <upthere@yandex.ru>
2022-03-16 21:16:45 +0300

    refactor: add Section class

--- scripts/Section.js
+++ scripts/Section.js
@@ -9,11 +9,7 @@ export default class Section {
     this._initialArray.forEach(item => this._renderer(item));
   }
 
-  addItem(element, isAppend) {
-    if (isAppend) {
-      this._container.append(element);
-    } else {
-      this._container.prepend(element);
-    }
+  addItem(element) {
+    this._container.append(element);
   }
 }
